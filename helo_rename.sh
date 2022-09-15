c2aab343e603ff0848eace01265cb34a068128c9
hendogg02 <michael.henson@me.com>
2022-09-15 16:53:12 -0400

    Extra info for editor.
    Added extra comments as well as variable naming to title Middle School and High School records.

--- helo_rename.sh
+++ helo_rename.sh
@@ -1,5 +1,33 @@
 #!/bin/bash
 
+# Custom variables
+# Campus naming and IP
+cmpsInit="ZZ"
+cmpsIP="10.0.0.1"
+
+# To change the time hhmmss are all ran together. This is military time. If it is 8 o'clock do not write 08 but rather just 8. 
+# Add one minute to the record time to give buffer for seconds past.
+BeforeGeneralName="25500"
+GeneralName="30100"
+
+BeforeFirstService="84500"
+FirstService="90100"
+
+BeforeSecondService="104500"
+SecondService="110100"
+
+BeforeThirdService="124500"
+ThirdService="130100"
+
+BeforeMiddleSchool="154500"
+MiddleSchool="160100"
+MiddleSchoolName="Middle School"
+
+BeforeHighSchool="184500"
+HighSchool="190100"
+HighSchoolName="High School"
+
+# Current Day calculator
 Date=$(date +"%m-%d-%Y")
 Day=$(date +"%a")
 Hour=$(date +"%H")
@@ -25,30 +53,8 @@ if [[ $FirstTimeCharacter -eq "0" ]]
     then FullTime=${FullTime:1}
 fi
 
-# Campus naming and IP
-cmpsInit="ZZ"
-cmpsIP="10.0.0.1"
 
-# To change the time hhmmss are all ran together. This is military time. If it is 8 o'clock do not write 08 but rather just 8. 
-# Add one minute to the record time to give buffer for seconds past.
-BeforeGeneralName="25500"
-GeneralName="30100"
-
-BeforeFirstService="84500"
-FirstService="90100"
-
-BeforeSecondService="104500"
-SecondService="110100"
-
-BeforeThirdService="124500"
-ThirdService="130100"
-
-BeforeMiddleSchool="154500"
-MiddleSchool="160100"
-
-BeforeHighSchool="184500"
-HighSchool="190100"
-
+# Renaming scripts
 if [[ $Day == "Sun" ]]
     then 
         if [[ $BeforeFirstService -gt $FullTime ]]
@@ -89,7 +95,7 @@ if [[ $Day == "Sun" ]]
             elif [[ $FullTime -le $MiddleSchool ]]
             then
                 Time="4"
-                Extension=%20"-"%20"(MiddleSchool)"
+                Extension=%20"-"%20"(${MiddleSchoolName})"
         fi
 fi
 
@@ -100,12 +106,14 @@ if [[ $Day == "Wed" ]]
             elif [[ $FullTime -le $HighSchool ]]
             then
                 Time="7"
-                Extension=%20"-"%20"(HighSchool)"
+                Extension=%20"-"%20"(${HighSchoolName})"
         fi
 fi
 
+# The AJA Filename Prefix based on above rename variables.
 prefix="${cmpsInit}"%20"${Date}"%20"${Time}${AP}"%20"${Day}${Extension}"
 
+# Top of the day generic naming for crash record.
 if [[ $BeforeGeneralName -gt $FullTime ]]
     then echo "Before 3am"
     elif [[ $FullTime -le $GeneralName ]]
@@ -119,6 +127,7 @@ echo "Current timestamp is: $Date $Time$AP $Day"
 echo ""
 sleep 1s
 
+# Command sent to AJA Helo.
 curl "http://$cmpsIP/config?action=set&paramid=eParamID_FilenamePrefix&value=$prefix"
 sleep 1s
 
