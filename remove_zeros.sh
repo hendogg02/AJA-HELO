b981a14bfad8e502b3d51ffeb966a27a0a0fde90
hendogg02 <michael.henson@me.com>
2022-09-19 10:35:51 -0400

    corrected to what the current is

--- remove_zeros.sh
+++ remove_zeros.sh
@@ -7,11 +7,12 @@ source ${dir}/config_me.sh
 ## Looking at a filename of ZZ 08-14-2022 9AM Sun - (Sun Review)_001_000001.mp4 and removing the zeros.
 # ${f: -4} is grabbing the last 4 characters of a string.
 
-for f in /volume1/Review/$cmpsInit/HELO/*; do
+for f in /volume1/Review/$cmpsInit/preHELO/*; do
 
     if [[ "$f" == *"_001"* ]]
         then
-            mv "$f" "${f%%\_001*}${f: -4}";
+            filename="${f##*preHELO/}"
+            mv "$f" "${f%\/preHELO/*}/HELO/${filename%\_001*}${f: -4}";
     fi      
 
 done
