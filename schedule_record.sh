c7d9fe8727b09a7e78ad2a2e73f3658d5b26f829
hendogg02 <michael.henson@me.com>
2022-10-03 12:33:16 -0400

    Create schedule_record.sh

---
+++ schedule_record.sh
@@ -0,0 +1,13 @@
+ #!/bin/bash
+
+################################
+# THIS SCRIPT SETS THE SCHEDULER TO RECORD ONLY
+# OPTION 1: RECORD
+# OPTION 2: STREAM
+# OPTION 3: RECORD + STREAM
+################################
+
+source config_me.sh
+
+curl "http://'$heloIP'/config?action=set&paramid=eParamID_SchedulerActivity&value=1"
+echo "$cmpsInit Helo scheduler has been set to record only."
